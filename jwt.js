0f23a477552fde3e2a55672e0389d67c91d38b8d
Coding rev <fernando.lobo.prez@gmail.com>
2025-04-17 01:41:53 -0300

    07-JWT basics
    07-JWT basics

--- jwt.js
+++ jwt.js
@@ -1,7 +1,9 @@
-const secret = 'supersecretpass'
-const tokenHash = 'username' + secret;
+const jwt = require('jsonwebtoken');
 
-const user = {
-    id:1,
-    token:tokenHash
-}
+let id = '100';
+const secret = 'supersecret';
+
+const token = jwt.sign(id,secret);
+const decodedToken = jwt.verify('eyJhbGciOiJIUzI1NiJ9.MTAw._p4CTP9MejT8kntvqaJI0HR_jo2DLfuLaRqCIahBf54',secret);
+
+console.log(decodedToken)
